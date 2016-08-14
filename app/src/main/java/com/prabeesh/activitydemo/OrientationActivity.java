e503281f77c3689af622fb1b5dad010533ca9439
Karan <karan.kumar.c@gmail.com>
2016-08-14 17:33:01 +0530

    Handling orientation change Setting background gradient Using different layout files for different orientation

--- app/src/main/java/com/prabeesh/activitydemo/OrientationActivity.java
+++ app/src/main/java/com/prabeesh/activitydemo/OrientationActivity.java
@@ -21,10 +21,50 @@ public class OrientationActivity extends AppCompatActivity {
         editText = (EditText) findViewById(R.id.edit_text);
         textView = (TextView) findViewById(R.id.textView);
         button = (Button) findViewById(R.id.button);
+
     }
 
     public void onClick(View view) {
         button.setText("Logout");
         textView.setText("Testing text");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        button.setText(savedInstanceState.getString("buttonText"));
+        textView.setText(savedInstanceState.getString("textViewText"));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("buttonText", button.getText().toString());
+        outState.putString("textViewText", textView.getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
